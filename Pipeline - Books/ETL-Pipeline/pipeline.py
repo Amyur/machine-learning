e6cec2bdc1315af9f6c3ef2db089cb05ac50087f
AMYLKAR URREA <amylkar.urrea@udea.edu.co>
2021-03-07 18:59:19 -0500

    upt3

--- Pipeline - Books/ETL-Pipeline/pipeline.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.call(['python', 'extract.py'], cwd="./Extract")
-subprocess.call(['python', 'transform.py'], cwd="./Transform")
-subprocess.call(['python', 'main.py'], cwd="./Load")
